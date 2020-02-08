d280b7614c66407e16622f564c91a3ee8cd8b53e
mehmetyildiz7 <yildzmehmet7@gmail.com>
2020-02-08 18:25:49 +0300

    Default values added to some inputs

--- AdessoRideShare.Web/Inputs/SetTravelPlanStatusInput.cs
+++ AdessoRideShare.Web/Inputs/SetTravelPlanStatusInput.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace AdessoRideShare.Web.Inputs
 {
     public class SetTravelPlanStatusInput
     {
+        [DefaultValue(1)]
         public int TravelPlanId { get; set; }
+        [DefaultValue(false)]
         public bool IsActive { get; set; }
     }
 }
